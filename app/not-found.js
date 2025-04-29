5b7339f8cbf302786023e7be05a2c9014bdefc96
Bezaleel Ashefor <3107111+knightbenax@users.noreply.github.com>
2025-04-29 00:32:47 -0400

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div style={{ padding: '4rem', textAlign: 'center' }}>
+        <h1 style={{ fontSize: '3rem' }}>404 - Page Not Found</h1>
+        <p style={{ marginTop: '1rem' }}>Sorry, we couldn't find what you were looking for.</p>
+      </div>
+    );
+  }
+  
