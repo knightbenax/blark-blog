d02651aa432d352d0c684f115d046d346b4fe9b4
Bezaleel Ashefor <3107111+knightbenax@users.noreply.github.com>
2026-01-21 21:16:06 -0500

    stash

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
     basePath: '/blog',
     trailingSlash: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'getbasin.app',
+            },
+            {
+                protocol: 'https',
+                hostname: 'miro.medium.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
